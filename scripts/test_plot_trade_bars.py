5d9cddb286d95b97975f028a4b6c7bcba5439d06
Nicholas <nskg@pm.me>
2021-08-27 10:19:21 +0200

    plots of daily bars

--- scripts/test_plot_trade_bars.py
+++ scripts/test_plot_trade_bars.py
@@ -1,10 +1,12 @@
 import unittest
-from plots import plot_price
+from plot_price import plot_price
+
 
 class TestCase(unittest.TestCase):
     def test_plot_bars(self):
-        test_args = ["test_data/ABC_NYSE@0_daily_bars.csv", "out"]
+        test_args = ["test_data/daily_bars.png","test_data/ABC_NYSE@0_daily_bars.csv","test_data/DEF_NYSE@0_daily_bars.csv"]
         plot_price(test_args)
+
 
 if __name__ == '__main__':
     unittest.main()
